4b783715ea41bb7e96f8a4159ac2edc232f8d9d5
Swaraj Panigrahi <swapanigrahi@deloitte.com>
2020-11-12 12:08:54 +0530

    Test user config

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,7 +4,9 @@ This is a functional component which shows a order summary for the user.
 import React from 'react';
 
 const OrderSummary = (props) => {
-
+    return (
+        <div></div>
+    );
 }
 
 export default OrderSummary;
